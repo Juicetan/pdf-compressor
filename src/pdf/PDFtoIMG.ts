8f85fe26a42585092658248bfbdbf0cfd0e53ad6
Juicetan <me@justinlam.ca>
2025-06-20 23:24:18 -0400

    implement parallel rendering and added manifest and icons;

--- src/pdf/PDFtoIMG.ts
+++ src/pdf/PDFtoIMG.ts
@@ -1,38 +1,66 @@
 import {
-    createCanvasContext,
-    createCanvasFromViewport
+  createCanvasContext,
+  createCanvasFromViewport
 } from "../util/File";
 import { IOptions } from './PDFCompress';
+import Deferred from '../models/deferred';
 
 const convertPDFtoIMG = (pdfDocumentProxy, options: IOptions) => {
-    return renderAllPages(pdfDocumentProxy, options);
+  return renderAllPages(pdfDocumentProxy, options);
 }
 
-const renderAllPages = async (file, options: IOptions) => {
-    let images = [];
+const renderAllPages = (pdfDocumentProxy, options: IOptions) => {
+  var def = new Deferred();
+  let images = [];
+  let completed = 0;
+  const maxConcurrency = 4;
+  let curConcurrency = 0;
+  let queue = Array.from({
+    length: pdfDocumentProxy.numPages
+  }, (_, i) => i + 1);
 
-    for (let i = 1; i <= file.numPages; i++) {
-        let pdfPageProxy = await file.getPage(i);
-        let renderedPage = await renderPage(pdfPageProxy, options);
-        images.push(JSON.parse(JSON.stringify(renderedPage)));
+  const process = async function(){
+    if(completed === pdfDocumentProxy.numPages){
+      images = images.filter((img) => img !== undefined)
+      console.log('> done images', images);      
+      def.resolve(images);
+      return;
+    } else if(curConcurrency >= maxConcurrency || !queue.length){
+      return;
     }
+    
+    curConcurrency++;
+    const pageNum = queue.pop();
+    let pdfPageProxy = await pdfDocumentProxy.getPage(pageNum);
+    
+    if(queue.length && curConcurrency < maxConcurrency){
+      process();
+    }
+    
+    let renderedPage = await renderPage(pdfPageProxy, options);
+    images[pageNum - 1] = JSON.parse(JSON.stringify(renderedPage));
+    completed++;
+    curConcurrency--;
+    process();
+  }
+  process();
 
-    return images;
+  return def.promise;
 }
 
 const renderPage = async (pdfPageProxy, options: IOptions) => {
-    const pageViewport = pdfPageProxy.getViewport({scale: options.scale});
-    const canvas = createCanvasFromViewport(pageViewport);
-    const canvasContext = createCanvasContext(canvas);
-    const renderTaskParams = {canvasContext: canvasContext, viewport: pageViewport}
+  const pageViewport = pdfPageProxy.getViewport({ scale: options.scale });
+  const canvas = createCanvasFromViewport(pageViewport);
+  const canvasContext = createCanvasContext(canvas);
+  const renderTaskParams = { canvasContext: canvasContext, viewport: pageViewport }
 
-    await pdfPageProxy.render(renderTaskParams).promise;
+  await pdfPageProxy.render(renderTaskParams).promise;
 
-    const base64 = canvas.toDataURL("image/jpeg", options.quality);
+  const base64 = canvas.toDataURL("image/jpeg", options.quality);
 
-    canvas.remove();
+  canvas.remove();
 
-    return base64;
+  return base64;
 };
 
 export default convertPDFtoIMG;
